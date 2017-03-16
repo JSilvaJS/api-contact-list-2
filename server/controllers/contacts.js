4914f0767864e4117f91038ed75896713c295f30
Jonathan Silva <silva.jon86@gmail.com>
2017-03-16 08:04:57 -0400

    initial commit

--- server/controllers/contacts.js
+++ server/controllers/contacts.js
@@ -1,6 +1,7 @@
 const Contacts = require("../models").Contacts
 
 module.exports = {
+
 	create (req, res) {
 		Contacts.create ({
 			name: req.body.name,
@@ -11,6 +12,33 @@ module.exports = {
 			email: req.body.email
 		})
 		.then(contact => res.status(201).send(contact))
-		.catch(error => res.status(400).send(error))
+		.catch(error => res.status(400).send(error));
 	},
-}
+
+	listSingle (req, res) {
+		Contacts.findAll({
+			where: {
+				id: req.params.id
+			}
+		})
+			.then(contacts => res.status(201).send(contacts))
+			.catch(error => res.status(400).send(error));
+	},
+
+	listAll (req, res) {
+		Contacts.findAll()
+		.then(contact => res.status(200).send(contact))
+		.catch(error => res.status(500).send(error));
+	},
+
+	update (req, res) {
+		Contacts.findById(req.params.id)
+		.then(contact => {
+			contact.update(req.body, {
+				fields: ['name', 'phone_number', 'city', 'state', 'photo_url', 'email']
+			})
+				.then(rowsChanged => res.status(202).send(contact))
+				.catch(error => res.status(400).send(error));
+		})
+	}
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,11 @@ const ContactsController = require("../controllers/contacts")
 module.exports = (app) => {
   // Add your routes here
   app.post('/contacts', ContactsController.create);
+  app.get('/contacts', ContactsController.listAll);
+  app.get('/contacts/:id', ContactsController.listSingle);
+  app.put('/contacts/:id', ContactsController.update);
 };
+
+// https://glacial-chamber-70166.herokuapp.com/  LIVE SERVER to use with POSTMAN, $http
+
+//https://git.heroku.com/glacial-chamber-70166.git GIT REPO for the server on Heroku
